f18e6ba6d93df1ac9120423f3720ad46811d27b9
Malte-Maurice Dreyer <dreyer.maltem+dev@gmail.com>
2015-03-21 23:48:43 +0000

    Add build scripts

---
+++ Gruntfile.js
@@ -0,0 +1,64 @@
+/* jshint node:true */
+
+"use strict";
+
+module.exports = function (grunt) {
+    // Show elapsed time at the end
+    require("time-grunt")(grunt);
+    // Load all grunt tasks
+    require("load-grunt-tasks")(grunt);
+
+    // Project configuration.
+    grunt.initConfig({
+        config: {
+            main: "url",
+            global: "Url"
+        },
+        babel: {
+            options: {
+                sourceMap: true
+            },
+            dist: {
+                files: {
+                    "dist/cjs.js": "<%= config.main %>.js"
+                }
+            }
+        },
+        browserify: {
+            dist: {
+                options: {
+                    browserifyOptions: {
+                        standalone: "<%= config.global %>"
+                    }
+                },
+                files: {
+                    "dist/browser.js": "dist/cjs.js"
+                }
+            }
+        },
+        uglify: {
+            dist: {
+                options: {
+                    screwIE8: true
+                },
+                files: {
+                    "dist/<%= config.main %>.min.js": "<%= config.main %>.js"
+                }
+            },
+            distCjs: {
+                files: {
+                    "dist/cjs.min.js": "dist/cjs.js"
+                }
+            },
+            distBrowser: {
+                files: {
+                    "dist/browser.min.js": "dist/browser.js"
+                }
+            }
+        }
+    });
+
+    grunt.task.registerTask("build:es6", ["uglify:dist"]);
+    grunt.task.registerTask("build:cjs", ["babel:dist", "uglify:distCjs"]);
+    grunt.task.registerTask("build:browser", ["babel:dist", "browserify:dist", "uglify:distBrowser"]);
+};
